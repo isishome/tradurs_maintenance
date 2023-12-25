3592381fce8a72956e598283a1f37dc4e446618a
unknown <sarah0603@kongjin.sen.es.kr>
2023-12-25 18:47:05 +0900

    update party play

--- src/i18n/en/index.ts
+++ src/i18n/en/index.ts
@@ -2,7 +2,7 @@ export default {
   notice: {
     title: 'System Maintenance Announcement',
     top: 'Hello. This is Tradurs.\n\nIn order to provide more stable services, we will be performing system maintenance as described below.\nAs a result, the Tradurs site and the game sites provided by Tradurs will be unavailable for a period of time.\nservices may be unavailable for a period of time.',
-    contents: 'Inspection date: October 17, 2023 (Tue) 09:00 ~ Upon completion of inspection\nInspection target : Tradurs server\nImpact : All services will be unavailable',
+    contents: 'Inspection date: December 25, 2023 (Mon) 10:00 ~ Upon completion of inspection\nInspection target : Tradurs server\nImpact : All services will be unavailable',
     bottom: 'We will notify you again when the server is normalized.\n\nThank you.'
   }
 }
