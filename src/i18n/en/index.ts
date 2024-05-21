71771687cc3a0aad3ea11fff4be4129d2fea091e
isishome <isishome@hanmail.net>
2024-05-21 18:05:32 +0900

    m

--- src/i18n/en/index.ts
+++ src/i18n/en/index.ts
@@ -2,7 +2,7 @@ export default {
   notice: {
     title: 'System Maintenance Announcement',
     top: 'Hello. This is Tradurs.\n\nIn order to provide more stable services, we will be performing system maintenance as described below.\nAs a result, the Tradurs site and the game sites provided by Tradurs will be unavailable for a period of time.\nservices may be unavailable for a period of time.',
-    contents: 'Inspection date: May 20, 2024 (Sat) 20:20 ~ Upon completion of inspection\nInspection target : Tradurs server\nImpact : All services will be unavailable',
+    contents: 'Inspection date: ~ Upon completion of inspection\nInspection target : Tradurs server\nImpact : All services will be unavailable',
     bottom: 'We will notify you again when the server is normalized.\n\nThank you.'
   }
 }
